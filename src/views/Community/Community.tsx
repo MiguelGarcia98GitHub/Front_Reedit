d285305c1aba76e23d0c2e28ee9fb9c82f2d92a9
MiguelGarcia98Github <miguelgarciard98@gmail.com>
2023-07-10 21:16:23 +0200

    Updated .gitignore

--- src/views/Community/Community.tsx
+++ src/views/Community/Community.tsx
@@ -56,7 +56,7 @@ const Community = () => {
   }
 
   useEffect(() => {
-    fetchCommunityData().then((communityDataPossibleError) => {});
+    fetchCommunityData().then(() => {});
   }, []);
 
   useEffect(() => {
